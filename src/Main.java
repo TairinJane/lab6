03f25f70b0cd89bf16f042cd89d12a6d46bd986a
tairinjane <tairinjane@gmail.com>
2019-05-22 18:55:06 +0300

    helios version 0.1.1 exception on start handle

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        if (args[0].equalsIgnoreCase("server")) {
-            try {
-                int port = Integer.parseInt(args[1]);
-                new Thread(new Server(port)).start();
-            } catch (NumberFormatException e) {
-                System.out.println("Usage: (server | client <host>) <port>");
-            }
-        } else if (args[0].equalsIgnoreCase("client")) {
-            try {
-                String host = args[1];
-                int port = Integer.parseInt(args[2]);
-                new Thread(new Client(host, port)).start();
-            } catch (NumberFormatException e) {
-                System.out.println("Usage: (server | client <host>) <port>");
+        if (args.length > 1) {
+            if (args[0].equalsIgnoreCase("server")) {
+                try {
+                    int port = Integer.parseInt(args[1]);
+                    new Thread(new Server(port)).start();
+                } catch (NumberFormatException e) {
+                    System.out.println("Usage: (server | client <host>) <port>");
+                }
+            } else if (args[0].equalsIgnoreCase("client")) {
+                try {
+                    String host = args[1];
+                    int port = Integer.parseInt(args[2]);
+                    new Thread(new Client(host, port)).start();
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("Usage: (server | client <host>) <port>");
+                }
             }
         } else {
             System.out.println("Usage: (server | client <host>) <port>");
